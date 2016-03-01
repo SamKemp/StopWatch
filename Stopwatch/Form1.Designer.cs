9772c337a7f97709937d75533929ea09237b23eb
untamemadman <modding@untamemadman.pw>
2016-03-01 14:59:45 +0000

    Intitial Commit

---
+++ Stopwatch/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labTime = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labSecTime = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.buttClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Split";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(427, 105);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start/Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(228, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 94);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.BackColor = System.Drawing.Color.Transparent;
+            this.labTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labTime.Location = new System.Drawing.Point(240, 36);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(140, 153);
+            this.labTime.TabIndex = 3;
+            this.labTime.Text = "0";
+            this.labTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 153);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ".";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labSecTime
+            // 
+            this.labSecTime.AutoSize = true;
+            this.labSecTime.BackColor = System.Drawing.Color.Transparent;
+            this.labSecTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labSecTime.Location = new System.Drawing.Point(83, 36);
+            this.labSecTime.Name = "labSecTime";
+            this.labSecTime.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.labSecTime.Size = new System.Drawing.Size(140, 153);
+            this.labSecTime.TabIndex = 5;
+            this.labSecTime.Text = "0";
+            this.labSecTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(691, 419);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(458, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 354);
+            this.listBox1.TabIndex = 7;
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // buttClose
+            // 
+            this.buttClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttClose.Location = new System.Drawing.Point(458, 372);
+            this.buttClose.Name = "buttClose";
+            this.buttClose.Size = new System.Drawing.Size(222, 35);
+            this.buttClose.TabIndex = 9;
+            this.buttClose.Text = "Close";
+            this.buttClose.UseVisualStyleBackColor = true;
+            this.buttClose.Click += new System.EventHandler(this.buttClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 417);
+            this.Controls.Add(this.buttClose);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labSecTime);
+            this.Controls.Add(this.labTime);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labSecTime;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button buttClose;
+    }
+}
+
---
+++ Stopwatch/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        public int _running= 0;
+        public int _counter = 0;
+        public int _counter2 = 00;
+        public int _toggle = 0;
+        public int _clocktype = 0;
+
+        List<string> _items = new List<string>(); // <-- Add this
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 100;
+            timer2.Interval = 50;
+            timer3.Interval = 10;
+        }
+
+        private void startTimers()
+        {
+            timer1.Start();
+            timer3.Start();
+            _running = 1;
+        }
+
+        private void stopTimers()
+        {
+            timer1.Stop();
+            timer3.Stop();
+            _running = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_running == 0)
+            {
+                startTimers();
+            }
+            else
+            {
+                stopTimers();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _counter ++;
+
+            if (_counter > 9)
+            {
+                _counter = 0;
+                _counter2++;
+            }
+
+            labTime.Text = _counter.ToString();
+
+            if (_counter2 >= 10)
+            {
+                this.labSecTime.Location = new Point(7, 36);
+                if (_counter2 >= 100)
+                {
+                    this.labSecTime.Location = new Point(-65, 36);
+                }
+            }
+            else
+            {
+                this.labSecTime.Location = new Point(83, 36);
+            }
+
+            labSecTime.Text = _counter2.ToString();
+
+            if (_counter2 == 99)
+            //if (_counter2 == 10)
+            {
+                stopTimers();
+                MessageBox.Show("You should go outside and get a propper stopwatch!", "An error occured");
+                timer2.Start();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _items.Add(_counter2 + "." + _counter);
+            //MessageBox.Show(_counter2.ToString()+ "." + _counter.ToString(), "Split");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (_running == 1)
+            {
+                stopTimers();
+            }
+
+            Reset();
+        }
+
+        private void Reset()
+        {
+            _counter = 0;
+            _counter2 = 00;
+
+            _items.Clear();
+            listRefresh();
+
+            timer2.Stop();
+            pictureBox1.BackColor = Color.White;
+
+            labTime.Text = _counter.ToString();
+            labSecTime.Text = _counter2.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (_toggle == 1)
+            {
+                pictureBox1.BackColor = Color.Red;
+                _toggle = 0;
+            }
+            else
+            {
+                pictureBox1.BackColor = Color.White;
+                _toggle = 1;
+            }
+
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (_items.Count == 16)
+            {
+                _items.RemoveAt(0);
+            }
+            listRefresh();
+        }
+
+        private void listRefresh()
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = _items;
+        }
+
+        private void buttClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
